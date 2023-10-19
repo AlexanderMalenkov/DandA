701eb04ed456d62fb865e76de421988780dc6b3f
Alexander Malenkov <malenkov2001@gmail.com>
2023-10-19 23:34:41 +0300

    DANDA-100500: Fix slider

--- danda/src/app/components/Slider/Slider.tsx
+++ danda/src/app/components/Slider/Slider.tsx
@@ -57,10 +57,262 @@ const Slider = ({ type }: { type: "first" | "second" }) => {
               height="410"
               description="Organic Delicacies"
             />
+             <SliderItem
+              image={img1}
+              width="327"
+              height="327"
+              description="Potted flowers"
+            />
+            <SliderItem
+              image={img2}
+              width="356"
+              height="475"
+              description="Organic products"
+            />
+            <SliderItem
+              image={img3}
+              width="546"
+              height="360"
+              description="Seasonal Arrangements"
+            />
+            <SliderItem
+              image={img4}
+              width="356"
+              height="475"
+              description="Fresh Flowers"
+            />
+            <SliderItem
+              image={img5}
+              width="356"
+              height="327"
+              description="Floristry Services"
+            />
+            <SliderItem
+              image={img6}
+              width="356"
+              height="410"
+              description="Organic Delicacies"
+            />
+             <SliderItem
+              image={img1}
+              width="327"
+              height="327"
+              description="Potted flowers"
+            />
+            <SliderItem
+              image={img2}
+              width="356"
+              height="475"
+              description="Organic products"
+            />
+            <SliderItem
+              image={img3}
+              width="546"
+              height="360"
+              description="Seasonal Arrangements"
+            />
+            <SliderItem
+              image={img4}
+              width="356"
+              height="475"
+              description="Fresh Flowers"
+            />
+            <SliderItem
+              image={img5}
+              width="356"
+              height="327"
+              description="Floristry Services"
+            />
+            <SliderItem
+              image={img6}
+              width="356"
+              height="410"
+              description="Organic Delicacies"
+            />
+             <SliderItem
+              image={img1}
+              width="327"
+              height="327"
+              description="Potted flowers"
+            />
+            <SliderItem
+              image={img2}
+              width="356"
+              height="475"
+              description="Organic products"
+            />
+            <SliderItem
+              image={img3}
+              width="546"
+              height="360"
+              description="Seasonal Arrangements"
+            />
+            <SliderItem
+              image={img4}
+              width="356"
+              height="475"
+              description="Fresh Flowers"
+            />
+            <SliderItem
+              image={img5}
+              width="356"
+              height="327"
+              description="Floristry Services"
+            />
+            <SliderItem
+              image={img6}
+              width="356"
+              height="410"
+              description="Organic Delicacies"
+            />
+             <SliderItem
+              image={img1}
+              width="327"
+              height="327"
+              description="Potted flowers"
+            />
+            <SliderItem
+              image={img2}
+              width="356"
+              height="475"
+              description="Organic products"
+            />
+            <SliderItem
+              image={img3}
+              width="546"
+              height="360"
+              description="Seasonal Arrangements"
+            />
+            <SliderItem
+              image={img4}
+              width="356"
+              height="475"
+              description="Fresh Flowers"
+            />
+            <SliderItem
+              image={img5}
+              width="356"
+              height="327"
+              description="Floristry Services"
+            />
+            <SliderItem
+              image={img6}
+              width="356"
+              height="410"
+              description="Organic Delicacies"
+            />
           </div>
         ) : (
           <div className={styles.gridContainer}>
             <SliderItem
+              image={img7}
+              width="327"
+              height="327"
+              description="Organic Wines"
+            />
+            <SliderItem
+              image={img8}
+              width="356"
+              height="475"
+              description="European delicacies"
+            />
+            <SliderItem
+              image={img9}
+              width="546"
+              height="360"
+              description="Meat delicacies"
+            />
+            <SliderItem
+              image={img10}
+              width="356"
+              height="475"
+              description="Coffee"
+            />
+            <SliderItem
+              image={img11}
+              width="356"
+              height="327"
+              description="Sweets and candies"
+            />
+            <SliderItem
+              image={img12}
+              width="356"
+              height="410"
+              description="Сhocolate delights"
+            />
+             <SliderItem
+              image={img7}
+              width="327"
+              height="327"
+              description="Organic Wines"
+            />
+            <SliderItem
+              image={img8}
+              width="356"
+              height="475"
+              description="European delicacies"
+            />
+            <SliderItem
+              image={img9}
+              width="546"
+              height="360"
+              description="Meat delicacies"
+            />
+            <SliderItem
+              image={img10}
+              width="356"
+              height="475"
+              description="Coffee"
+            />
+            <SliderItem
+              image={img11}
+              width="356"
+              height="327"
+              description="Sweets and candies"
+            />
+            <SliderItem
+              image={img12}
+              width="356"
+              height="410"
+              description="Сhocolate delights"
+            />
+             <SliderItem
+              image={img7}
+              width="327"
+              height="327"
+              description="Organic Wines"
+            />
+            <SliderItem
+              image={img8}
+              width="356"
+              height="475"
+              description="European delicacies"
+            />
+            <SliderItem
+              image={img9}
+              width="546"
+              height="360"
+              description="Meat delicacies"
+            />
+            <SliderItem
+              image={img10}
+              width="356"
+              height="475"
+              description="Coffee"
+            />
+            <SliderItem
+              image={img11}
+              width="356"
+              height="327"
+              description="Sweets and candies"
+            />
+            <SliderItem
+              image={img12}
+              width="356"
+              height="410"
+              description="Сhocolate delights"
+            />
+             <SliderItem
               image={img7}
               width="327"
               height="327"
